aae2648adba95286dfec0c4476f41e8791e41179
Chris Stead <cmstead@gmail.com>
2017-07-08 10:02:06 -0700

    First pass creating a project skeleton

---
+++ generators/app/index.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const path = require('path');
+const Generator = require('yeoman-generator');
+
+function joinPath(basePathTokens, filename) {
+    return basePathTokens.concat([filename]).join(path.sep);
+}
+
+module.exports = class extends Generator {
+    prompting() {
+        const prompts = [
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'Project name:'
+            },
+            {
+                type: 'input',
+                name: 'projectVarName',
+                message: 'Module variable name:'
+            },
+            {
+                type: 'input',
+                name: 'projectDescription',
+                message: 'Project description'
+            }
+        ];
+
+        return this.prompt(prompts).then(props => {
+            this.props = props;
+        });
+    }
+
+    writing() {
+        const projectName = this.props.projectName;
+
+        this.destinationRoot(projectName);
+
+        const templateValues = {
+            projectName: projectName,
+            projectVarName: this.props.projectVarName,
+            projectDescription: this.projectDescription
+        };
+
+        let testPathTokens = ['test'];
+        let testUtilsPathTokens = testPathTokens.concat(['test-utils']);
+
+        const copyDefinitions = [
+            {
+                templatePath: joinPath(testPathTokens, 'test.template.js'),
+                filePath: joinPath(testPathTokens, projectName + '.test.js'),
+                isTemplate: true
+            },
+            {
+                templatePath: 'index.template.js',
+                filePath: 'index.js',
+                isTemplate: true
+            },
+            {
+                templatePath: 'package.template.json',
+                filePath: 'package.json',
+                isTemplate: true
+            },
+            {
+                templatePath: 'README.template.md',
+                filePath: 'README.md',
+                isTemplate: true
+            },
+            {
+                templatePath: '.editorconfig',
+                filePath: '.editorconfig',
+                isTemplate: false
+            },
+            {
+                templatePath: '.eslintrc',
+                filePath: '.eslintrc',
+                isTemplate: false
+            },
+            {
+                templatePath: '.gitignore',
+                filePath: '.gitignore',
+                isTemplate: false
+            },
+            {
+                templatePath: '.npmignore',
+                filePath: '.npmignore',
+                isTemplate: false
+            },
+            {
+                templatePath: 'gulpfile.template.js',
+                filePath: 'gulpfile.js',
+                isTemplate: true
+
+            },
+            {
+                templatePath: 'signet-types.js',
+                filePath: 'signet-types.js',
+                isTemplate: false
+            },
+            {
+                templatePath: joinPath(testUtilsPathTokens, 'approvals-config.js'),
+                filePath: joinPath(testUtilsPathTokens, 'approvals-config.js'),
+                isTemplate: false
+            },
+            {
+                templatePath: joinPath(testUtilsPathTokens, 'prettyJson.js'),
+                filePath: joinPath(testUtilsPathTokens, 'prettyJson.js'),
+                isTemplate: false
+            }
+        ];
+
+        copyDefinitions.forEach(copyDefinition => {
+            if (copyDefinition.isTemplate) {
+                this.fs.copyTpl(
+                    this.templatePath(copyDefinition.templatePath),
+                    this.destinationPath(copyDefinition.filePath),
+                    templateValues
+                );
+            } else {
+                this.fs.copy(
+                    this.templatePath(copyDefinition.templatePath),
+                    this.destinationPath(copyDefinition.filePath)
+                );
+            }
+        });
+
+    }
+
+    install() {
+        this.installDependencies({
+            npm: true,
+            bower: false,
+            yarn: false
+        });
+    }
+};
---
+++ generators/app/templates/test/test.template.js
@@ -0,0 +1,19 @@
+'use strict';
+
+if (typeof exploreFunction !== 'function') {
+    require('quokka-signet-explorer').before();
+}
+
+const assert = require('chai').assert;
+const prettyJson = require('./test-utils/prettyJson');
+const sinon = require('sinon');
+
+const <%= projectVarName %> = require('../index.js');
+
+describe('<%= projectName %>', function () {
+    require('./test-utils/approvals-config');
+});
+
+if (typeof global.runQuokkaMochaBdd === 'function') {
+    runQuokkaMochaBdd();
+}
