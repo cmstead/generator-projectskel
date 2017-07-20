2d04ed5dce95b073f8b1415c43ccf3430e365ad7
Chris Stead <cmstead@gmail.com>
2017-07-20 12:50:20 -0700

    Updated templates to use matchlight

--- generators/app/templates/index.template.js
+++ generators/app/templates/index.template.js
@@ -3,15 +3,16 @@
 
     if (isNode) {
         const signet = require('./signet-types');
+        const matchlight = require('matchlight')(signet);
 
-        module.exports = moduleFactory(signet);
+        module.exports = moduleFactory(signet, matchlight.match);
     } else if (typeof signet === 'object') {
-        window.<%= projectVarName %> = moduleFactory(signet);
+        window.<%= projectVarName %> = moduleFactory(signet, matchlight.match);
     } else {
         throw new Error('The module <%= projectName %> requires Signet to run.');
     }
 
-})(function (signet) {
+})(function (signet, match) {
     'use strict';
 
 
--- generators/module/templates/module.template.js
+++ generators/module/templates/module.template.js
@@ -4,11 +4,14 @@
     const isNode = typeof module !== 'undefined' && typeof module.exports !== undefined;
 
     if(isNode) {
-        module.exports = moduleFactory();
+        const signet = require('./signet-types');
+        const matchlight = require('matchlight')(signet);
+
+        module.exports = moduleFactory(signet, matchlight.match);
     } else {
-        window.<%= fileVarName %> = moduleFactory();
+        window.<%= projectVarName %> = moduleFactory(signet, matchlight.match);
     }
 
-})(function () {
+})(function (signet, match) {
     // source code here
 });
